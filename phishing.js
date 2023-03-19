5d089ff132f61f09f9985d82053807afda467938
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-03-19 20:33:36 +0800

    Update phishing.js

--- phishing.js
+++ phishing.js
@@ -1,3 +1,14 @@
 document.addEventListener('contextmenu',function(event){
    event.preventDefault();
 },true);
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    alert("瀏覽器不支援定位功能");
+}
+
+function showPosition(position) {
+    alert("緯度: " + position.coords.latitude + 
+          "，經度: " + position.coords.longitude);
+}
