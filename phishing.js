32c5d0c5898621ee1217a4ef5c6fc39622af5729
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-03-19 23:12:36 +0800

    Update phishing.js

--- phishing.js
+++ phishing.js
@@ -2,20 +2,33 @@ document.addEventListener('contextmenu',function(event){
    event.preventDefault();
 },true);
 
-if ("geolocation" in navigator) {
-  // 支持定位功能
-  navigator.geolocation.getCurrentPosition(function(position) {
-    // 獲取到位置信息
-    var latitude = position.coords.latitude; // 緯度
-    var longitude = position.coords.longitude; // 經度
+// if ("geolocation" in navigator) {
+//   // 支持定位功能
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     // 獲取到位置信息
+//     var latitude = position.coords.latitude; // 緯度
+//     var longitude = position.coords.longitude; // 經度
     
-    // 在這裡可以將位置信息發送到後端進行處理，例如：
-    // $.post('/api/position', {latitude: latitude, longitude: longitude});
-  }, function(error) {
-    // 無法獲取位置信息
-    console.log('定位失敗：' + error.message);
-  });
+//     // 在這裡可以將位置信息發送到後端進行處理，例如：
+//     // $.post('/api/position', {latitude: latitude, longitude: longitude});
+//   }, function(error) {
+//     // 無法獲取位置信息
+//     console.log('定位失敗：' + error.message);
+//   });
+// } else {
+//   // 不支持定位功能
+//   console.log('您的瀏覽器不支持定位功能。');
+// }
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
 } else {
-  // 不支持定位功能
-  console.log('您的瀏覽器不支持定位功能。');
+  alert("Your browser doesn't support geolocation.");
+}
+
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 }
