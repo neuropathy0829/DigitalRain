0a4f14360a2a78671649c09f504dabe57eae158b
neuropathy <96810159+neuropathy0829@users.noreply.github.com>
2023-04-06 21:17:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -68,6 +68,22 @@ window.onresize=()=>{
 
 setInterval(draw, root.matrixspeed);
 
+//禁用右鍵
 document.addEventListener('contextmenu',function(event){
-   event.preventDefault();
-},true);
+    event.preventDefault();
+ },true);
+//禁用F12、Ctrl+Shift+I、Ctrl+Shift+J、Ctrl+U
+document.onkeydown = function(e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+}
